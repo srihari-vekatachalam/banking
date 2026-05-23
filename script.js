0720014f67ac915dc39341485d73a65ff2588d23
srihari-vekatachalam <srih75028@gmail.com>
2026-05-23 11:16:20 +0530

    fixed fetch url

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    const response = await fetch("http://localhost:3000/login", {
+    const response = await fetch("/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
